bf880535afef97307bad33c378daef7fb5c8ae45
Nick Nogai <nnogai@spindletop.software>
2017-04-19 09:38:58 +0600

    router

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from '../components/App';
+import App from '../pages/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
